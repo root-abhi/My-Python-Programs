7b2009119579374ed8abf2369e2b8c3829240c90
Abhishek Chauhan <Abhishek.Chauhan@darkmatter.ae>
2019-08-16 13:39:16 +0400

    my python learning2

---
+++ seven_index.py
@@ -0,0 +1,10 @@
+email =  'This is Python Class'
+print(email[0]) ## This will print first index which would be "T" from This word
+print(email[2]) ## This will print "i"
+
+
+## You can also do Negative Indexing, In Megative Index it starts counting from the end of line. eg:
+
+email = 'This is first email'
+print(email[-2]
+print(email[-4])
---
+++ six.py
@@ -0,0 +1,4 @@
+email = ''' Hi All
+This is the first scripr written to use tree quote
+Thank you for reading the message'''
+print(email)
